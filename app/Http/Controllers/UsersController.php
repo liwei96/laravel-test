712b7ab123a51a0fb77543203373fd8775c2e4fe
unknown <1070138814@qq.com>
2020-05-05 15:48:37 +0800

    '登录成功后导航逻辑'

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,6 +26,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
+        Auth::login($user);
         session()->flash('success','欢迎你，注册成功');
         return redirect()->route('users.show',[$user]);
     }
